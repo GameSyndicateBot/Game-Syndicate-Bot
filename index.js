dd4d522c2f45d4c07c66c2a85b0de3a04c75c30c
GameSyndicateBot <fufik232323@mail.ru>
2026-07-21 14:18:59 +0300

    fix22

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
+const fightButtons = require('./events/fightButtons');
 const { startScheduler } = require('./utils/scheduler');
-const { startAutoEvents } = require('./systems/quickEventSystem');
 require('dotenv').config();
 
 const { buildInfo } = require('./utils/buildInfo');
@@ -58,12 +59,12 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     startScheduler(client);
-    startScheduler(client); startScheduler(client); startScheduler(client);
-    startAutoEvents(client);
+    
     console.log(`✅ Бот ${client.user.tag} запущен!`);
 });
 
 client.on('interactionCreate', async interaction => {
+    await fightButtons(interaction);
     try {
         // Все визуальные панели используют серверный ник участника.
         const { attachServerDisplayName } = require('./utils/displayName');
